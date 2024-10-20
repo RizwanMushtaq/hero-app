4a23eb545eaf58bdad34e44a2081fc4b4d3cda0a
Rizwan <rizwanmushtaq15@gmail.com>
2024-10-20 22:55:47 +0200

    disabling public build folder for ec2 workaround

---
+++ public/build/assets/ApplicationLogo-aHaCBYlU.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./hero-logo-new-nav-DErDjNTI.js";import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o as s,f as t,b as a}from"./app-Bwa3TLRK.js";const c={},n={class:"image-wrapper"};function i(_,o){return s(),t("div",n,o[0]||(o[0]=[a("img",{src:r},null,-1)]))}const l=e(c,[["render",i]]);export{l as A};
---
+++ public/build/assets/hero-logo-new-nav-DErDjNTI.js
@@ -0,0 +1,1 @@
+const s="/build/assets/hero-logo-new-nav-2AhTH5pW.svg";export{s as _};
